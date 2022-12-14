71d0c5ca4c2341cc02bca27647d0ae2b1ae2c374
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-12-14 15:55:19 -0800

    Template Start

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+WebApplication webapp = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!webapp.Environment.IsDevelopment())
+{
+    webapp.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    webapp.UseHsts();
+}
+
+webapp.UseHttpsRedirection();
+webapp.UseStaticFiles();
+
+webapp.UseRouting();
+
+webapp.UseAuthorization();
+
+webapp.MapRazorPages();
+
+webapp.Run();
+
