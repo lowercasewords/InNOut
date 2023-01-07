32214e2f2deb40be868b95dfe1b26f765ddb627c
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2023-01-07 14:11:44 -0800

    index page beta

---
+++ Pages/InPage.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InNOut.Pages.Shared
+{
+	public class InPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
