64a5b5c08605b49c8014c5e41055a3dc595cfc22
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2023-08-04 14:46:34 -0700

    ensure-changes

--- Pages/InPage.cshtml.cs
+++ Pages/InPage.cshtml.cs
@@ -5,12 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace InNOut.Pages.Shared
+namespace InNOut.Pages
 {
 	public class InPageModel : PageModel
     {
+        public readonly ILogger<InPageModel> _logger;
+
+        public ILogger<InPageModel> Logger
+        {
+            get
+            {
+                return _logger;
+            }
+        }
+
+        public InPageModel(ILogger<InPageModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            
         }
     }
 }
