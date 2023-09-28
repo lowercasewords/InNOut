02e1d308b4234bd9fe38662801517541c083d723
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2023-09-28 15:13:16 -0700

    changing POST destionation url

--- Pages/InPage.cshtml.cs
+++ Pages/InPage.cshtml.cs
@@ -5,10 +5,12 @@ namespace InNOut.Pages
 {
 	public class InPageModel : PageModel
     {
+        private static readonly string IN_DESTINATION = "MessageStorage.txt";
+
         //Appends clients data to the database 
         public void OnPost(string message_title, string message_content)
         {
-            using (StreamWriter streamWriter = new StreamWriter("MessageStorage.txt", true))
+            using (StreamWriter streamWriter = new StreamWriter(IN_DESTINATION, true))
             {
                 streamWriter.WriteLine($"{message_title}:{message_content};");
                 streamWriter.Close();
